b0cc7bbb4772a9da74e6393e36e43ab6784e99a8
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2020-12-02 21:30:41 -0400

    Update conexion_db.php

--- conexion_db.php
+++ conexion_db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","db_xodux");
+$conex = mysqli_connect("mysql://b41876607bfdde:033ffd11@us-cdbr-east-02.cleardb.com/heroku_08b07b505008bb1?reconnect=true","b41876607bfdde","033ffd11","heroku_08b07b505008bb1");
 
 ?>
